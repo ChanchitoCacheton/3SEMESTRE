583423001e2560fe9deeccd688b390b2c6961db4
JZentenoM <31105001+JZentenoM@users.noreply.github.com>
2018-05-18 08:22:56 -0500

    3enraya

--- main.cpp
+++ main.cpp
@@ -5,32 +5,85 @@ using namespace std;
 void imprimirC(char (*A)[3][3]){
     char*i=**A;
     for(;i<**A+27;i++){
-            cout << *i;
+            if((int(i)- int(A))%3==0)
+                cout <<endl;
+            if((int(i)- int(A))%9==0)
+                cout <<endl;
+
+            cout << " " << *i ;
         }
+        cout << endl;
 
 }
 
-void jugada(int x, int y, int z){
 
+void jugada(char (*A)[3][3], int x, int y, int z, int jugador){
+    char*i=**A;
+    if (jugador==1){
+        i=i+((9*x)+(3*y)+z);
+        *i='X';
+    }
 
+    else if (jugador==2){
+        i=i+((9*x)+(3*y)+z);
+        *i='O';
+    }
 }
+
 
 int main()
 {
-    char C[3][3][3]={{{'0','1','2'},
-                      {'3','4','a'},
-                      {'8','7','6'}},
+    char C[3][3][3]={{{'_','_','_'},
+                      {'_','_','_'},
+                      {'_','_','_'}},
 
-                     {{'a','b','c'},
-                      {'d','e','f'},
-                      {'g','h','i'}},
+                     {{'_','_','_'},
+                      {'_','_','_'},
+                      {'_','_','_'}},
 
-                     {{'A','B','C'},
-                      {'D','E','F'},
-                      {'G','H','I'}}};
+                     {{'_','_','_'},
+                      {'_','_','_'},
+                      {'_','_','_'}}};
+    int x=0;
+    int y=0;
+    int z=0;
 
-    int x,y,z;
+    while(true){
+
+    cout << "Ingrese jugada del Jugador 1: " << endl;
+    cin >> x;
+    cin >> y;
+    cin >> z;
+    cout << endl;
+    if (x>2 || y>2 || z>2){
+        cout << endl << "JUGADA INVALIDA - PIERDE TURNO" << endl;
+    }
+
+    else{
+    jugada(C,x,y,z,1);
     imprimirC(C);
-    cout <<endl <<  int(C);
+    }
+
+
+    cout << endl << "**********************************************" << endl << endl;
+
+    cout << "Ingrese jugada del Jugador 2: " << endl;
+    cin >> x;
+    cin >> y;
+    cin >> z;
+    cout << endl;
+
+    if (x>2 || y>2 || z>2)
+        cout << endl << "JUGADA INVALIDA" << endl;
+
+    else{
+    jugada(C,x,y,z,2);
+    imprimirC(C);
+    cout << endl << "**********************************************" << endl << endl;
+    }
+
+    }
+
+
     return 0;
 }
