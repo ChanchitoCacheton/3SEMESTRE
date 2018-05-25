c455a417196505191b356e6fbd545804aa051191
JZentenoM <31105001+JZentenoM@users.noreply.github.com>
2018-05-25 06:14:39 -0500

    3enraya2

--- main.cpp
+++ main.cpp
@@ -9,25 +9,49 @@ void imprimirC(char (*A)[3][3]){
                 cout <<endl;
             if((int(i)- int(A))%9==0)
                 cout <<endl;
-
             cout << " " << *i ;
         }
         cout << endl;
-
 }
-
 
 void jugada(char (*A)[3][3], int x, int y, int z, int jugador){
     char*i=**A;
-    if (jugador==1){
         i=i+((9*x)+(3*y)+z);
-        *i='X';
-    }
 
-    else if (jugador==2){
-        i=i+((9*x)+(3*y)+z);
-        *i='O';
-    }
+        if(*i=='_'){
+            if (jugador==1)
+                *i='X';
+            else if (jugador==2)
+                *i='O';
+        }
+        else
+        cout << endl << "JUGADA INVALIDA - PIERDE TURNO" << endl;
+
+}
+
+bool ganador (char (*A)[3][3]){
+    char*i = **A;
+
+    if (*i!='_' && *i==*(i+4) && *(i+4)==*(i+8)) return true;
+    else if (*i!='_' &&  *i==*(i+13) && *(i+13)==*(i+26)) return true;
+    else if (*i!='_' && *i==*(i+12) && *(i+12)==*(i+24)) return true;
+    else if (*i!='_' && *i==*(i+10) && *(i+10)==*(i+20)) return true;
+
+    else if (*(i+2)!='_' && *(i+2)==*(i+4) && *(i+4)==*(i+6)) return true;
+    else if (*(i+2)!='_' && *(i+2)==*(i+13) && *(i+13)==*(i+24)) return true;
+    else if (*(i+2)!='_' && *(i+2) == *(i+14) && *(i+14)==*(i+26)) return true;
+    else if (*(i+2)!='_' && *(i+2) == *(i+10) && *(i+10)==*(i+18)) return true;
+
+    else if (*(i+9)!='_' && *(i+9) == *(i+13) && *(i+13)==*(i+17)) return true;
+    else if (*(i+9)!='_' && *(i+9) == *(i+13) && *(i+13)==*(i+17)) return true;
+
+    else if(*(i+3)!= '_' && *(i+3) == *(i+13) && *(i+13)==*(i+23)) return true;
+    else if(*(i+6)!= '_' && *(i+6) == *(i+16) && *(i+16)==*(i+26)) return true;
+    else if (*(i+11)!='_' && *(i+11)== *(i+13) && *(i+13) == *(i+15)) return true;
+    else if (*(i+18)!='_' && *(i+18)==*(i+22) && *(i+22)==*(i+26)) return true;
+    else if (*(i+20)!='_' && *(i+20)==*(i+22) && *(i+22)==*(i+24)) return true;
+
+    else return false;
 }
 
 
@@ -50,40 +74,43 @@ int main()
 
     while(true){
 
-    cout << "Ingrese jugada del Jugador 1: " << endl;
-    cin >> x;
-    cin >> y;
-    cin >> z;
-    cout << endl;
-    if (x>2 || y>2 || z>2){
-        cout << endl << "JUGADA INVALIDA - PIERDE TURNO" << endl;
-    }
+    cout << "TURNO DE JUGADOR 1: " << endl;
 
-    else{
+    do{cout << "x: "; cin >> x;}while(x>2);
+    do{cout << "y: "; cin >> y;}while(y>2);
+    do{cout << "z: "; cin >> z;}while(z>2);
+
+
     jugada(C,x,y,z,1);
     imprimirC(C);
-    }
 
+    if(ganador(C)){
+        cout << endl << "//////////////////////" << endl
+                     << "/-- GANA JUGADOR 1 --/" << endl
+                     << "//////////////////////" << endl;
+        break;
+    }
 
     cout << endl << "**********************************************" << endl << endl;
 
-    cout << "Ingrese jugada del Jugador 2: " << endl;
-    cin >> x;
-    cin >> y;
-    cin >> z;
-    cout << endl;
+    cout << "TURNO DE JUGADOR 2: " << endl;
 
-    if (x>2 || y>2 || z>2)
-        cout << endl << "JUGADA INVALIDA" << endl;
+    do{cout << "x: "; cin >> x;}while(x>2);
+    do{cout << "y: "; cin >> y;}while(y>2);
+    do{cout << "z: "; cin >> z;}while(z>2);
 
-    else{
     jugada(C,x,y,z,2);
     imprimirC(C);
+
+    if(ganador(C)){
+        cout << endl << "//////////////////////" << endl
+                     << "/-- GANA JUGADOR 2 --/" << endl
+                     << "//////////////////////" << endl;
+        break;
+    }
+
     cout << endl << "**********************************************" << endl << endl;
     }
-
-    }
-
 
     return 0;
 }
